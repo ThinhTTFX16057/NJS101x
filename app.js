549fd2d314f412be6e4dd6c0d9e31d349c20dfbf
= <thinhttfx16057@funix.edu.vn>
2022-05-21 10:39:54 +0700

    send request

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const http = require('http');
 const server = http.createServer(
-    (req, res) => {console.log(req)}
+    (req, res) => {console.log(req.url, req.method, req.headers)
+    res.setHeader('Contente-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>');
+    res.end();
+    }
 );
 server.listen(3000)
