cd8654e362053de27f6a47ce9069f185553e0839
= <thinhttfx16057@funix.edu.vn>
2022-05-23 09:54:25 +0700

    how middleware works

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ app.use((req, res, next)=>{
     console.log('In the middleware!');
     next(); //allows the request to continue to the next middleware in line
 });
+
 app.use((req, res, next)=>{
-    console.log('In another middleware!')
-    //..
-})
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>')
+    
+});
+
 const server = http.createServer(app);
 
 server.listen(3000)
