3892b3f99d5ade1929de6a71f07d82f282912c54
= <thinhttfx16057@funix.edu.vn>
2022-05-23 09:58:52 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+
 
 const express=require('express');
 
@@ -15,6 +15,4 @@ app.use((req, res, next)=>{
     
 });
 
-const server = http.createServer(app);
-
-server.listen(3000)
+app.listen(3000)
