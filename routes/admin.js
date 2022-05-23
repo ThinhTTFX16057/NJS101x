ebd8de595b9cbf5f8ed37e93c24054cf67fa7368
= <thinhttfx16057@funix.edu.vn>
2022-05-23 19:20:07 +0700

    Converting HTML Files to Pug

--- routes/admin.js
+++ routes/admin.js
@@ -8,13 +8,13 @@ const router = express.Router();
 
 const products = [];
 //admin/add-product => GET
-router.get('/add-product',(req, res, next)=>{
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
+router.get('/add-product', (req, res, next)=>{
+    res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 //admin/add-product => POST
 router.post('/add-product',(req, res, next)=>{
-    products.push({title: req.body.title})
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
