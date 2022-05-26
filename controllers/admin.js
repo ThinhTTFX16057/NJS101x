924df329f3d6117922dbb2c546ede7dd7aee9b72
= <thinhttfx16057@funix.edu.vn>
2022-05-26 14:07:43 +0700

    Pre-Populating the Edit Product Page with Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false
   });
 };
 
@@ -20,10 +21,15 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if(!editMode){return res.redirect('/')}
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editting: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId,product=>{
+    if(!product){return res.redirect('/');}
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
   });
 };
 
