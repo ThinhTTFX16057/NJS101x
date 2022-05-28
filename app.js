1c13fd2246f9503acbf04c22237f414833c2cde9
= <thinhttfx16057@funix.edu.vn>
2022-05-28 23:22:39 +0700

    Resetting the Cart & Fetching and Outputting Orders

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const CartItem = require('./models/cart-items');
 const Order = require('./models/order');
 const OrderItem = require('./models/order-item');
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,17 +20,16 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use( (req, res, next)=>{
-    User.findByPk(1)
-        .then(user =>{
-            req.user=user;
-            next();
-        })
-        .catch(err=>console.log(err));
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -39,37 +37,36 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User,{constraint: true, onDelete: 'CASCADE'});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 Order.belongsTo(User);
 User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem});
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-    .sync({force: true})
-    //.sync()
-    .then(result =>{
-        return User.findByPk(1);
-        //console.log(result);
-    })
-    .then(user => {
-        if(!user){
-            return User.create({name: 'Thinh', email: 'test@gmail.com'});
-        }
-        return user;
-    })
-    .then(user=>{
-        //console.log(user);
-        return user.createCart();
-        
-    })
-    .then(cart =>{
-        app.listen(3000);
-    })
-    .catch(err => console.log(err));
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Thinh', email: 'test@gmail.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node-complete', 'root', 'Thinh.med93', {dialect: 'mysql', host: 'localhost'});
+
+const sequelize = new Sequelize('node-complete', 'root', 'Thinh.med93', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
 module.exports = sequelize;
