e65ddc22ef240acc01978d8976da65195303a6ac
= <thinhttfx16057@funix.edu.vn>
2022-05-30 00:42:35 +0700

    One Note About Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
-const mongodb = require('mongodb');
+
 const Product = require('../models/product');
 
-const ObjectId = mongodb.ObjectId;
+
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -57,7 +57,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   
-  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, new ObjectId(prodId));
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
   return product
     .save()
     .then(result => {
