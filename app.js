95923a7df75326294f084052c42ea9c3b65a3c6e
= <thinhttfx16057@funix.edu.vn>
2022-05-31 03:34:53 +0700

    Adding and Using a User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('6293b639c404770d89840932')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('629529d304fc4a68e7a90af4')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,5 +34,14 @@ app.use(errorController.get404);
 
 mongoose
   .connect('mongodb+srv://thinhtran:pass123@cluster0.njciaw0.mongodb.net/shop')
-  .then(result => {app.listen(3000)})
+  .then(result => {
+    User.findOne().then(user =>{
+      if (!user) {
+        const user = new User({name: 'Thinh', email: 'test@test.com', cart:{items: []}});
+        user.save();
+      }
+    })
+    
+    app.listen(3000);
+  })
   .catch(err => console.log(err));
