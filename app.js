a41d42918bd7ddc60353f19c2b5cd2bde5568fa6
= <thinhttfx16057@funix.edu.vn>
2022-05-30 21:33:13 +0700

    Connecting to the MongoDB Server with Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +32,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client =>{
-  app.listen(3000);
-})
+mongoose
+  .connect('mongodb+srv://thinhtran:pass123@cluster0.njciaw0.mongodb.net/test')
+  .then(result => {app.listen(3000)})
+  .catch(err => console.log(err));
