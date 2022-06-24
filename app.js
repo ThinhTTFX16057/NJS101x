c1a0b93e855d688ecaa27fb2dad951c1b01415b7
= <thinhttfx16057@funix.edu.vn>
2022-06-24 14:37:48 +0700

    heroku build

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use((req, res) => {res.status(404).render('404', {pageTitle: 'Page Not Found
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000, '0.0.0.0', ()=>{console.log('Server is running!')});
   })
   .catch(err => {
     console.log(err);
